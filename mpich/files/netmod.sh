846543a0662214f3e9ed65c0a930774098db817f
Asa Rentschler <rentschleraj@ornl.gov>
2026-02-06 10:58:44 -0500

    Updated environment configuration for mpich.

--- mpich/files/netmod.sh
+++ mpich/files/netmod.sh
@@ -1,14 +1,34 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# Detect network type and set MPICH netmod
-if [ -z "${SLURM_PROCID+x}" ] || [ "${SLURM_PROCID}" -eq 0 ]; then
-    if ls /sys/class/net | grep -q "hsn[0-9]"; then
-        export MPIR_CVAR_CH4_NETMOD=ofi;
-        echo "Setting MPIR_CVAR_CH4_NETMOD=ofi for Slingshot";
-    elif ls /sys/class/net | grep -q "ib[0-9]"; then
-        export MPIR_CVAR_CH4_NETMOD=ucx;
-        echo "Setting MPIR_CVAR_CH4_NETMOD=ucx for InfiniBand";
+# check if MPIR_CVAR_CH4_NETMOD has already been defined
+if [ -z "${MPIR_CVAR_CH4_NETMOD}" ]; then
+    # export SLURM_PROCID if it exists
+    for kv in "${__exported_env__}"; do
+        case "${kv}" in
+            SLURM_PROCID=*)
+                export "${kv}";
+                break;
+                ;;
+        esac;
+    done;
+
+    # detect network type and set MPIR_CVAR_CH4_NETMOD
+    # only print action if SLURM_PROCID undefined or equal to 0
+    if [ "${SLURM_PROCID:-0}" = "0" ]; then
+        if ls /sys/class/net | grep -q "hsn[0-9]"; then
+            export MPIR_CVAR_CH4_NETMOD=ofi;
+            echo "Setting MPIR_CVAR_CH4_NETMOD=ofi for Slingshot";
+        elif ls /sys/class/net | grep -q "ib[0-9]"; then
+            export MPIR_CVAR_CH4_NETMOD=ucx;
+            echo "Setting MPIR_CVAR_CH4_NETMOD=ucx for InfiniBand";
+        else
+            echo "Cannot determine network type; MPIR_CVAR_CH4_NETMOD unchanged";
+        fi;
     else
-        echo "Cannot determine network type; MPIR_CVAR_CH4_NETMOD unchanged";
+        if ls /sys/class/net | grep -q "hsn[0-9]"; then
+            export MPIR_CVAR_CH4_NETMOD=ofi;
+        elif ls /sys/class/net | grep -q "ib[0-9]"; then
+            export MPIR_CVAR_CH4_NETMOD=ucx;
+        fi;
     fi;
 fi;
